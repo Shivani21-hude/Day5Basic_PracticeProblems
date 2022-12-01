2ae766860ca5490b26ff9c9c107f4bfa69ab14cd
Shivani21-hude <shivani21hude@gmail.com>
2022-12-01 14:53:48 +0530

    Problem of factor of prime numbers up to n

---
+++ Factor/Factor/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factor
+{
+    internal class Class2
+    {
+        static void factorization()
+        {
+            int n;
+        
+            Console.WriteLine("Enter number for to print prime number");
+            n=Convert.ToInt32(Console.ReadLine())
+            for (int i = 2; i <= n; i++)
+            {
+                fact = 1;
+                int count = 0;
+                for(int j = 2; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                    if (count >= 2)
+                    {
+                        break;
+                    }
+                    if (count < 2)
+                    {
+                        fact = fact * i;
+                    }
+                }Console.WriteLine(fact);
+            }
+        }
+    }
+}
---
+++ Factor/Factor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 class2= new Class2();
+            class2.factorization();
+        }
+    }
+}
