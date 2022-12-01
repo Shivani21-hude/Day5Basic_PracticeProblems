ba8365d2a4f3b81f1fc7fc5a567630caf65211db
Shivani21-hude <shivani21hude@gmail.com>
2022-12-01 19:46:16 +0530

    Problem to check number is even or odd

---
+++ Evenodd/Evenodd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("{0} is a even number",n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a odd number ", n);
+            }
+        }
+    }
+}
