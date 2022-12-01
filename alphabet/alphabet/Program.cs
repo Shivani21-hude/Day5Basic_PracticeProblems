61f0161ff39a6a54081690d6ac52e7d50280a64f
Shivani21-hude <shivani21hude@gmail.com>
2022-12-01 20:13:12 +0530

    Problem to check an alphabet is vowel or consonant

---
+++ alphabet/alphabet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a albhabet : ");
+            char c = Convert.ToChar(Console.ReadLine());
+            
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            {
+                Console.WriteLine("{0} is Vowel", c);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Consonant", c);
+            }
+        }
+    }
+}
