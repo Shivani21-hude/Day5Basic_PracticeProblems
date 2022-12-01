e370a2b3e618ff6f98e2a52b2b3f9f933c4f25b9
Shivani21-hude <shivani21hude@gmail.com>
2022-12-01 10:42:18 +0530

    adding leap year problem

---
+++ leapyear/leapyear/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapyear
+{
+    internal class Class1
+    {
+        public void year()
+        {
+            Console.WriteLine("enter year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else if (year % 100 == 0) 
+            {
+                Console.WriteLine("Not a leap year");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else
+            {
+                Console.WriteLine("not a leap year");
+            }
+        }
+    }
+}
