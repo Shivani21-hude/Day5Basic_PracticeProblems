629d3e33ff01d1ebb94e45a9f80d8370052f3b58
Shivani21-hude <shivani21hude@gmail.com>
2022-12-01 11:33:05 +0530

    Adding problem of Sum of harmoic number up to n

---
+++ harmonicNum/harmonicNum/Harmonic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonicNum
+{
+    internal class Harmonic
+    {
+        public double sumof()
+        {
+            Console.WriteLine("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            float s = 1 / 1;
+            for(int i = 2; i <= n; i++)
+            {
+                s = s + 1 / (float)i;
+            }
+            Console.WriteLine("The sum of harmonic numbers : ",s);
+        }
+    }
+}
---
+++ harmonicNum/harmonicNum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonicNum
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Harmonic harmonic = new Harmonic();
+             harmonic.sumof();
+        }
+    }
+}
+
