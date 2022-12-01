cac2c1598591ec226f7f9305d806b2b46712eab6
Shivani21-hude <shivani21hude@gmail.com>
2022-12-01 11:09:19 +0530

    Adding power of 2 to the power n

---
+++ PowerOf2/PowerOf2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf2
+{
+    internal class Class1
+    {
+        public void power()
+        {
+            Console.WriteLine("enter number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int value = 2;
+            for (int i = 1; i <= n; i++)
+            {
+                double p = Math.Pow(value, i);
+                Console.WriteLine("Power of 2 values are : ",p);
+                Console.ReadLine();
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
---
+++ PowerOf2/PowerOf2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.power();
+        }
+
+    }
+}
